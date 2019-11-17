6add5d63cd196a9cebc35e0a602d359703b23a85
Ben Stone <benjamin.h.stone@gmail.com>
2019-11-17 17:28:57 -0500

    implement output displaying button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import Keypad from './Keypad';
 import Output from './Output';
 
-function App() {
-  return (
-    <div className="App">
-      <Keypad buttonPressed = { (name) => console.log(name) }/>
-      <Output result = '0'/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.buttonPressed = this.buttonPressed.bind(this)
+  }
+
+  state = {
+    result: ''
+  }
+
+  buttonPressed(name) {
+    this.setState({
+      result: name
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Keypad buttonPressed = { this.buttonPressed }/>
+        <Output result = {this.state.result} />
+      </div>
+    );  
+  }
 }
 
 export default App;
